641180a80238229ea0ae908eba2b143256a083f5
Akhil Vinnakota <AkhilVinnakota@users.noreply.gitlab.com>
2024-07-03 12:14:16 +0530

    add api call in service

--- src/app/components/api-service/api-service.component.ts
+++ src/app/components/api-service/api-service.component.ts
@@ -1,18 +1,22 @@
 import { HttpClientModule, provideHttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../../services/users.service';
+import { JsonPipe } from '@angular/common';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-api-service',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [HttpClientModule, JsonPipe],
+  providers: [UsersService],
   templateUrl: './api-service.component.html',
   styleUrl: './api-service.component.scss',
 })
 export class ApiServiceComponent implements OnInit {
+  public data = [];
   constructor(private user: UsersService) {}
 
   ngOnInit(): void {
-    this.user.getUsers().subscribe((data) => console.log(data));
+    this.user.getUsers().subscribe((data) => (this.data = data as never[]));
   }
 }
