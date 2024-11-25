6f9e1bcc21c0838fde072d87b205bb24833a5143
Akhil Vinnakota <AkhilVinnakota@users.noreply.gitlab.com>
2024-11-25 12:50:01 +0530

    add new route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AgGridAngular } from 'ag-grid-angular';
 import { CounterComponent } from './components/counter/counter.component';
+import { ChangeDetectionComponent } from './components/change-detection/change-detection.component';
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [AgGridAngular, RouterModule, CounterComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+	selector: 'app-root',
+	standalone: true,
+	imports: [AgGridAngular, RouterModule, CounterComponent, ChangeDetectionComponent],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { CounterComponent } from './components/counter/counter.component';
 import { ApiServiceComponent } from './components/api-service/api-service.component';
 import { RootComponent } from './components/root/root.component';
+import { ChangeDetectionComponent } from './components/change-detection/change-detection.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'root', pathMatch: 'full' },
-  { path: 'root', component: RootComponent },
-  { path: 'counter', component: CounterComponent },
-  { path: 'services', component: ApiServiceComponent },
+	{ path: '', redirectTo: 'root', pathMatch: 'full' },
+	{ path: 'root', component: RootComponent },
+	{ path: 'counter', component: CounterComponent },
+	{ path: 'services', component: ApiServiceComponent },
+	{ path: 'changeDetection', component: ChangeDetectionComponent },
 ];
